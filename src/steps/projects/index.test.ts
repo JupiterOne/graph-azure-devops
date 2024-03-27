5e43cddcdddfba3c70a65e2cd669e1b5759b420c
bharatkk-metron <bharat.kumavat@metronlabs.com>
2024-03-27 10:17:25 +0530

    project unit test updated

--- src/steps/projects/index.test.ts
+++ src/steps/projects/index.test.ts
@@ -7,29 +7,27 @@ import { setupAzureRecording } from '../../../test/recording';
 import { buildStepTestConfigForStep, config } from '../../../test/config';
 import { getMatchRequestsBy } from '../../../test/getMatchRequestsBy';
 
-describe(Steps.FETCH_PROJECTS, () => {
-  let recording: Recording;
-  afterEach(async () => {
-    if (recording) {
-      await recording.stop();
-    }
-  });
-
-  test(
-    'Fetch Projects',
-    async () => {
-      recording = setupAzureRecording({
-        directory: __dirname,
-        name: 'fetch-projects',
-        options: {
-          matchRequestsBy: getMatchRequestsBy({ config: config }),
-        },
-      });
-
-      const stepConfig = buildStepTestConfigForStep(Steps.FETCH_PROJECTS);
-      const stepResult = await executeStepWithDependencies(stepConfig);
-      expect(stepResult).toMatchStepMetadata(stepConfig);
-    },
-    unit_test_custom_timeout,
-  );
+let recording: Recording;
+afterEach(async () => {
+  if (recording) {
+    await recording.stop();
+  }
 });
+
+test(
+  'Fetch Projects',
+  async () => {
+    recording = setupAzureRecording({
+      directory: __dirname,
+      name: 'fetch-projects',
+      options: {
+        matchRequestsBy: getMatchRequestsBy({ config: config }),
+      },
+    });
+
+    const stepConfig = buildStepTestConfigForStep(Steps.FETCH_PROJECTS);
+    const stepResult = await executeStepWithDependencies(stepConfig);
+    expect(stepResult).toMatchStepMetadata(stepConfig);
+  },
+  unit_test_custom_timeout,
+);
